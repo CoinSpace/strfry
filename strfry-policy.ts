10866455c3d52e6cd0716c9f11a47926609d4d1e
Wendel Matavelli <wendelmatavelli@gmail.com>
2024-06-28 19:10:09 -0300

    Testing rate limit policy

--- strfry-policy.ts
+++ strfry-policy.ts
@@ -13,7 +13,9 @@ for await (const msg of readStdin()) {
   const result = await pipeline(msg, [
     [hellthreadPolicy, { limit: 100 }],
     [antiDuplicationPolicy, { ttl: 60000, minLength: 50 }],
-    [rateLimitPolicy, { whitelist: ['127.0.0.1', '192.168.1.136', '192.168.1.1', '10.0.0.0','10.10.40.16', '10.115.40.33', '10.45.30.94', '10.80.30.35', '10.50.40.81', '10.20.30.22'] }],
+    [rateLimitPolicy, { whitelist: ['127.0.0.1', '192.168.1.136', '192.168.1.1', '10.0.0.0',
+      '10.10.40.38', '10.20.30.206', '10.45.30.172', '10.80.30.197', '10.50.40.230', '10.115.40.177'
+    ] }],
   ]);
 
   writeStdout(result);
