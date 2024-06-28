d69a8d7bda004b47f5d75ae1257847587c0d0050
Wendel Matavelli <wendelmatavelli@gmail.com>
2024-06-28 18:28:08 -0300

    Testing rate limit policy

--- strfry-policy.ts
+++ strfry-policy.ts
@@ -13,10 +13,7 @@ for await (const msg of readStdin()) {
   const result = await pipeline(msg, [
     [hellthreadPolicy, { limit: 100 }],
     [antiDuplicationPolicy, { ttl: 60000, minLength: 50 }],
-    [rateLimitPolicy, { whitelist: ['127.0.0.1', '192.168.1.136', '192.168.1.1',
-       'servicediscovery-fcvgj8f4sc54.us-east-2-purplerelay.internal', 'servicediscovery-jcxbzsyteqcw.ap-northeast-1-purplerelay.internal', 
-       'servicediscovery-eyt5cvcn0kne.eu-central-1-purplerelay.internal', 'servicediscovery-y6tvlqmfuez7.af-south-1-purplerelay.internal', 
-       'servicediscovery-5u8kfcwtbmvm.sa-east-1-purplerelay.internal', 'servicediscovery-7fzr2wjxa4jr.us-east-1-purplerelay.internal'] }],
+    [rateLimitPolicy, { whitelist: ['127.0.0.1', '192.168.1.136', '192.168.1.1', '10.0.0.0','10.10.40.16', '10.115.40.33', '10.45.30.94', '10.80.30.35', '10.50.40.81', '10.20.30.22'] }],
   ]);
 
   writeStdout(result);
